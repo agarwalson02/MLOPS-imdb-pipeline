879025501cfe40b551fe028ae970f4b0e361cf28
agarwalson02 <agarwalson02@gmail.com>
2026-02-24 17:38:37 +0530

    Added model evaluation files

---
+++ src/model/model_evaluation.py
@@ -0,0 +1,102 @@
+import numpy as np
+import pandas as pd
+import pickle
+import json
+import sys
+from sklearn.metrics import accuracy_score,precision_score,recall_score,roc_auc_score
+import logging
+import mlflow
+import mlflow.sklearn
+from src.logger import logging
+from src.exception import MyException
+import dagshub
+
+mlflow.set_tracking_uri("https://dagshub.com/agarwalson02/MLOPS-imdb-pipeline.mlflow")
+dagshub.init(repo_owner='agarwalson02',repo_name='MLOPS-imdb-pipeline',mlflow=True)
+
+def load_model(file_path: str):
+    """Load the trained model from a file"""
+    try:
+        with open(file_path,'rb') as file:
+            model=pickle.load(file)
+        logging.info("Model loaded from %s",file_path)
+        return model
+    except FileNotFoundError:
+        raise MyException(f'File not found: {file_path}',sys)
+    except Exception as e:
+        raise MyException(f'Unexpected error occurred while loading the model:{e}',sys)
+
+
+def load_data(file_path: str) -> pd.DataFrame:
+    """Load data from a CSV file."""
+    try:
+        df = pd.read_csv(file_path)
+        logging.info('Data loaded from %s', file_path)
+        return df
+    except pd.errors.ParserError as e:
+        raise MyException(f'Failed to parse the CSV file:{e}',sys)
+    except Exception as e:
+        raise MyException(f'Unexpected error occurred while loading the data: {e}',sys)
+
+def evaluate_model(clf,X_test:np.ndarray,y_test:np.ndarray)->dict:
+    """Evaluate the model and return the evaluation metrics."""
+    try:
+        y_pred=clf.predict(X_test)
+        y_pred_prob=clf.predict_proba(X_test)[:,1]
+        
+        accuracy=accuracy_score(y_test,y_pred)
+        precision=precision_score(y_test,y_pred)
+        recall=recall_score(y_test,y_pred)
+        auc=roc_auc_score(y_test,y_pred)
+
+        metrics_dict={
+            'accuracy': accuracy,
+            'precision': precision,
+            'recall': recall,
+            'auc': auc
+        }
+        logging.info('Model evaluation metrics done')
+        return metrics_dict
+    except Exception as e:
+        raise MyException(f'Error while evaluating metrics {e}',sys)
+
+    
+def save_metrics(metrics:dict,file_path:str)->None:
+    """Save the evaluation metrics to a JSON file."""
+    try:
+        with open(file_path, 'w') as file:
+            json.dump(metrics, file, indent=4)
+        logging.info('Metrics saved to %s', file_path)
+    except Exception as e:
+        raise MyException(f'Error occurred while saving the metrics: {e}',sys)
+
+def save_model_info(run_id:str,model_path:str,file_path:str)->None:
+    """Save the model run ID and path to a JSON file."""
+    try:
+        model_info = {'run_id': run_id, 'model_path': model_path}
+        with open(file_path, 'w') as file:
+            json.dump(model_info, file, indent=4)
+        logging.debug('Model info saved to %s', file_path)
+    except Exception as e:
+        raise MyException(f'Error occurred while saving the model info: {e}',sys)
+
+def main():
+    try:
+        clf=load_model("./models/model.pkl")
+        test_data=load_data("./data/processed/test_bow.csv")
+
+        X_test=test_data.iloc[:,:-1].values
+        y_test=test_data.iloc[:,-1].values
+
+        metrics=evaluate_model(clf,X_test,y_test)
+        
+        save_metrics(metrics,'reports/metrics.json')
+
+    except Exception as e:
+        raise MyException(f'Failed to complete the model evaluation process {e}',sys)
+
+
+if __name__=="__main__":
+    main()
+
+
